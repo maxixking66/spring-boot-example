807353865f0ebfb491cb1038fb1038d12f5d81c9
maxixking66 <1993mohsen1993>
2021-11-04 09:07:23 +0330

    init project

---
+++ src/main/java/ir/maktab56/springbootexample/SpringBootExampleApplication.java
@@ -0,0 +1,13 @@
+package ir.maktab56.springbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/maktab56/springbootexample/SpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.maktab56.springbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
